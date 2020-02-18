844bab009e71ea296cc310b84bd7e5823b182bad
Gerrit Kitts <gkitts@gmail.com>
2020-02-18 15:35:34 -0500

    Moving condensed weapon to its own class.

---
+++ CleverGirl/CleverGirl/Objects/CondensedWeapon.cs
@@ -0,0 +1,30 @@
+﻿using BattleTech;
+using CustAmmoCategories;
+using System.Collections.Generic;
+
+namespace CleverGirl {
+
+    // A condensed weapon masquerades as the parent weapon, but keeps a list of all the 'real' weapons     
+    public class CondensedWeapon {
+        public int weaponsCondensed = 0;
+        public List<Weapon> condensedWeapons = new List<Weapon>();
+        public AmmoModePair ammoAndMode;
+
+        public CondensedWeapon() { }
+        public CondensedWeapon(Weapon weapon) {
+            AddWeapon(weapon);
+
+        }
+
+        // Invoke this after construction and every time you want to aggregate a weapon
+        public void AddWeapon(Weapon weapon) {
+            weaponsCondensed++;
+            this.condensedWeapons.Add(weapon);
+        }
+
+        public Weapon First {
+            get { return this.condensedWeapons[0]; }
+        }
+
+    }
+}
