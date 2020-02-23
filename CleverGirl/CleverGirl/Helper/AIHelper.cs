3a74022a3c5ce980a75c64799811dae6da77edeb
Gerrit Kitts <gkitts@gmail.com>
2020-02-23 09:55:15 -0500

    Attempting to check for ammo types

--- CleverGirl/CleverGirl/Helper/AIHelper.cs
+++ CleverGirl/CleverGirl/Helper/AIHelper.cs
@@ -195,13 +195,16 @@ namespace CleverGirl {
                     float armorReduction = 0f;
                     foreach(AmmunitionBox aBox in cWeapon.First.ammoBoxes) {
                         //Mod.Log.Debug($" -- Checking ammo box defId: {aBox.mechComponentRef.ComponentDefID}");
-                        if (aBox.mechComponentRef.Is<CleverGirlComponent>(out CleverGirlComponent cgComp) && cgComp.ArmorDamageReduction != 0) {
+                        if (aBox.ammoDef.Is.mechComponentRef.Def.Is<CleverGirlComponent>(out CleverGirlComponent cgComp) && cgComp.ArmorDamageReduction != 0) {
                             armorReduction = cgComp.ArmorDamageReduction;
                         }
                     }
                     if (armorReduction != 0f) {
                         Mod.Log.Debug($" -- APPLY DAMAGE REDUCTION OF: {armorReduction}");
                     }
+
+                    // TODO: AMS provides a shield to allies
+
 
                     // Need to precalc some values on every combatant - 
                     //  find objective targets
