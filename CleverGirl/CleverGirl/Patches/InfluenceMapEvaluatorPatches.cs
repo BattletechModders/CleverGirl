1a81a27e65fd13574b1d222f245405930becbd37
IceRaptor <gkitts@gmail.com>
2021-04-01 11:32:04 -0400

    - Inject custom influence factors into all influence maps

--- CleverGirl/CleverGirl/Patches/InfluenceMapEvaluatorPatches.cs
+++ CleverGirl/CleverGirl/Patches/InfluenceMapEvaluatorPatches.cs
@@ -15,11 +15,38 @@ namespace CleverGirl.Patches
 {
     // Replace the default InfluenceMapEvaluator to allow influenceFactors to be printed out for testing purposes
 
+	[HarmonyPatch(typeof(InfluenceMapEvaluator), MethodType.Constructor)]
+	static class InfluenceMapEvaluator_ctor
+    {
+		// Add custom 
+		static void Postfix(InfluenceMapEvaluator __instance, ref InfluenceMapAllyFactor[] ___allyfactors, 
+			ref InfluenceMapHostileFactor[] ___hostileFactors, ref InfluenceMapPositionFactor[] ___positionalFactors,
+			ref PreferHigherExpectedDamageToHostileFactor ___expectedDamageFactor)
+        {
+			List<InfluenceMapAllyFactor> allyFactors = new List<InfluenceMapAllyFactor>(___allyfactors);
+			Mod.Log.Info?.Write($"Adding {ModState.InfluenceMapAllyFactors.Count} custom ally factors to influence map.");
+			allyFactors.AddRange(ModState.InfluenceMapAllyFactors);
+			___allyfactors = allyFactors.ToArray();
+
+			List<InfluenceMapHostileFactor> hostileFactors = new List<InfluenceMapHostileFactor>(___hostileFactors);
+			Mod.Log.Info?.Write($"Adding {ModState.InfluenceMapHostileFactors.Count} custom hostile factors to influence map.");
+			hostileFactors.AddRange(ModState.InfluenceMapHostileFactors); 
+			___hostileFactors = hostileFactors.ToArray();
+
+			List<InfluenceMapPositionFactor> positionFactors = new List<InfluenceMapPositionFactor>(___positionalFactors);
+			Mod.Log.Info?.Write($"Adding {ModState.InfluenceMapPositionFactors.Count} custom position factors to influence map.");
+			positionFactors.AddRange(ModState.InfluenceMapPositionFactors);
+			___positionalFactors = positionFactors.ToArray();
+
+		}
+    }
+	
+
     [HarmonyPatch(typeof(InfluenceMapEvaluator), "RunEvaluationForSeconds")]
-    public static class InfluenceMapEvaluator_RunEvaluationForSeconds
+    static class InfluenceMapEvaluator_RunEvaluationForSeconds
     {
 
-        public static bool Prefix(InfluenceMapEvaluator __instance, float seconds, ref bool __result, 
+        static bool Prefix(InfluenceMapEvaluator __instance, float seconds, ref bool __result, 
 			ref GraphCoroutine ___evaluationCoroutine, bool ___evaluationComplete)
         {
             Mod.Log.Trace?.Write("AIU:CDT:Post");
